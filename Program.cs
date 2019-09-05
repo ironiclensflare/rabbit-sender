7fac2819e4bb96653bf5899fc701393265542dc9
Stuart Carruthers <stu@ironiclensflare.com>
2019-09-05 20:05:48 +0100

    Basic sender

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+using System;
+using System.Text;
+using RabbitMQ.Client;
 
 namespace rabbit_sender
 {
@@ -6,7 +8,22 @@ namespace rabbit_sender
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var factory = new ConnectionFactory { HostName = "localhost" };
+            using (var connection = factory.CreateConnection())
+            {
+                using (var channel = connection.CreateModel())
+                {
+                    channel.QueueDeclare("samplequeue", true, false, false, null);
+                    channel.BasicPublish("", "samplequeue", false, null, CreateMessage("Hello"));
+                    Console.WriteLine("Added message to the queue.");
+                }
+            }
+        }
+
+        static byte[] CreateMessage(string text)
+        {
+            var body = Encoding.UTF8.GetBytes(text);
+            return body;
         }
     }
 }
