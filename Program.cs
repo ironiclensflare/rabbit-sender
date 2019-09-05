07eda64d632084bdef072083a79083e5ab51c955
Stuart Carruthers <stu@ironiclensflare.com>
2019-09-05 19:24:22 +0100

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace rabbit_sender
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
